3ee4887feb492a214af13a06f30d9bd9078bf7cd
Vatricia Edgar <vatriciaedgar@gmail.com>
2019-09-22 13:58:52 -0700

    switch to student view if login successful

--- src/gui/LoginLogic.js
+++ src/gui/LoginLogic.js
@@ -2,16 +2,13 @@ async function sendLoginRequest(){
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    console.log("here");
-
     let loginSuccess = await UserDb.userExists(username, password);
-    console.log("here2");
     if(!loginSuccess){
         document.getElementById("loginFail").hidden = false;
-        console.log("fail");
     }
     else{
         document.getElementById("loginFail").hidden = true;
-        console.log("true");
+        document.getElementById("login").hidden = true;
+        document.getElementById("studentView").hidden = false;
     }
 }
