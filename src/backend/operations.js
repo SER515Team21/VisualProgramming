6804ac19bb0d1fe8bee679a4ac76bd3049ef512d
Matthew Frautnick <matthew.frautnick@gmail.com>
2019-09-22 14:14:47 -0700

    Added subtraction and addition operations

---
+++ src/backend/operations.js
@@ -0,0 +1,31 @@
+const Node = require('./Node');
+
+class Addition extends Node {
+
+    constructor(left,right) {
+        super();
+        this.left = left;
+        this.right = right;
+    }
+
+    get text() {
+        return this.left + " + " + this.right;
+    }
+}
+
+class Subtraction extends Node {
+
+    constructor(left,right) {
+        super();
+        this.left = left;
+        this.right = right;
+    }
+
+    get text() {
+        return this.left + " - " + this.right;
+    }
+}
+
+//Test
+//const lol = new Addition(3,3);
+//console.log(lol.text);
