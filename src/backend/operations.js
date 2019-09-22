ac0b08ddb9d01822174bde6b68930d7403e8ba33
Matthew Frautnick <matthew.frautnick@gmail.com>
2019-09-22 14:32:11 -0700

    Add multiplication and division operations

--- src/backend/operations.js
+++ src/backend/operations.js
@@ -26,6 +26,34 @@ class Subtraction extends Node {
     }
 }
 
+class Multiplication extends Node {
+
+    constructor(left,right) {
+        super();
+        this.left = left;
+        this.right = right;
+    }
+
+    get text() {
+        return this.left + " * " + this.right;
+    }
+}
+
+class Division extends Node {
+
+    constructor(left,right) {
+        super();
+        this.left = left;
+        this.right = right;
+    }
+
+    get text() {
+        return this.left + " / " + this.right;
+    }
+}
+
+
+
 //Test
 //const lol = new Addition(3,3);
 //console.log(lol.text);
