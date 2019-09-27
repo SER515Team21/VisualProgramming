2236993f480baf6ad793912c51ffe3246b2655b2
zlmonroe <zlmonroe@asu.edu>
2019-09-27 13:39:40 -0700

    Update the style requirements

---
+++ src/gui/accordianViewListeners.js
@@ -0,0 +1,15 @@
+/* global document */
+const acc = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        const panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        }
+        else {
+            panel.style.display = "block";
+        }
+    });
+}
