6ce4be67a470b8a36c0c22de756197c7ea67914a
JonathanBush <buoywestone@gmail.com>
2019-09-28 10:30:24 -0700

    Fixed several issues with NodeForest and applied style guide.

--- src/backend/NodeForest.js
+++ src/backend/NodeForest.js
@@ -1,38 +1,36 @@
 /**
- * This is an implementation of a Node Forest, which represents all node trees currently in the editor.
+ * This is an implementation of a Node Forest, which represents all node trees
+ * currently in the editor.
  *
  * @date 9/25/2019
  */
 
-class NodeForest{
-    static instance;
-    static treeCount = 0;
-    nodeForest = {}
+class NodeForest {
 
-    constructor(){
-        if(!NodeForest.instance) {
+    constructor() {
+        if (!NodeForest.instance) {
             NodeForest.instance = this;
+            NodeForest.treeCount = 0;
+            this.nodeForest = {};
         }
         return NodeForest.instance;
     }
 
-    getNode(nodeId){
+    getNode(nodeId) {
         return this.nodeForest[nodeId];
     }
 
-    insertNode(newNode){
-        let nodeId = NodeForest.treeCount
+    insertNode(newNode) {
         this.nodeForest[NodeForest.treeCount] = newNode;
-        NodeForest.treeCount ++;
-        return nodeId;
+        return NodeForest.treeCount++;
     }
 
-    updateNode(newNode, oldId){
+    updateNode(newNode, oldId) {
         this.nodeForest[oldId] = newNode;
         return oldId;
     }
 
-    deleteNode(nodeId){
+    deleteNode(nodeId) {
         delete this.nodeForest[nodeId];
     }
 }
