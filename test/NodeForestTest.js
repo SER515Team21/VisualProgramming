2263dd9174ce2a014d58b6c0cbfde78c447beb93
Vatricia Edgar <vatriciaedgar@gmail.com>
2019-09-30 17:56:45 -0700

    Get node fully tested

--- test/NodeForestTest.js
+++ test/NodeForestTest.js
@@ -4,14 +4,15 @@ const NumberNode = require("../src/backend/NumberNode.js");
 
 // addition node
 const addNode = new Operators.Addition(new NumberNode("4"), new NumberNode("2"));
-
 const addId = addNode.nodeId;
 // Parans Node (not yet implemented)
-
+wait = new Date(new Date().getTime() + 500);
+while(wait > new Date()){}
 // Multiplication Node
 const multiNode = new Operators.Multiplication(new NumberNode("3"), addNode);
 const multiId = multiNode.nodeId;
-
+wait = new Date(new Date().getTime() + 500);
+while(wait > new Date()){}
 // Final (Addition) Node
 
 const rootNode = new Operators.Addition(new NumberNode("1"), multiNode);
@@ -19,3 +20,6 @@ const rootId = rootNode.nodeId;
 
 NodeForest.insertRootNode(rootNode);
 
+const foundNode = NodeForest.getNode(addId, rootId);
+
+console.log(foundNode);
