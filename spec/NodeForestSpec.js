b7a27854e58f0d643e4323f3b1d8148e7457be97
zlmonroe <zlmonroe@asu.edu>
2019-10-01 14:55:16 -0700

    Added jasmine support. Fixed existing UT to use jasmine

---
+++ spec/NodeForestSpec.js
@@ -0,0 +1,42 @@
+/* global describe */
+/* global it */
+/* global expect */
+
+/* Imports for test */
+const NodeForest = require("../src/backend/NodeForest.js");
+const Operators = require("../src/backend/Operators.js");
+const NumberNode = require("../src/backend/NumberNode.js");
+
+/* Write the test suite */
+describe("Node Forest", () => {
+    /* A setup function
+    beforeEach(() => {
+    });
+    */
+
+    /* A specification */
+    it("shall be able to retrieve a node from an arbitrary root", () => {
+        // addition node
+        const addNode = new Operators.Addition(new NumberNode("4"), new NumberNode("2"));
+        const addId = addNode.nodeId;
+
+        let wait = new Date(new Date().getTime() + 500);
+        while (wait > new Date());
+
+        // Multiplication Node
+        const multiNode = new Operators.Multiplication(new NumberNode("3"), addNode);
+        const multiId = multiNode.nodeId;
+
+        wait = new Date(new Date().getTime() + 500);
+        while (wait > new Date());
+
+        // Final (Addition) Node
+        const rootNode = new Operators.Addition(new NumberNode("1"), multiNode);
+        const rootId = rootNode.nodeId;
+
+        NodeForest.insertRootNode(rootNode);
+        const foundNode = NodeForest.getNode(addId, rootId);
+
+        expect(foundNode.nodeId).toBe(addId);
+    });
+});
