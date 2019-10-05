16fcae281f701101c917ee0943ab5bb4c9ecc8a4
majluni <majluni@asu.edu>
2019-10-05 14:50:14 -0700

    Nodes are inseetable into other Nodes. Investigating why the original nodes are removed

--- src/gui/dragAndDrop.js
+++ src/gui/dragAndDrop.js
@@ -8,42 +8,82 @@
 /* global Operators */
 /* global NodeFactory */
 
+/**
+ * Sets data attribute to hold the id of the picked up HTML Node
+ * @param event
+ */
 function pickUpNode(event) {
     event.dataTransfer.setData("text", event.target.id);
-    event.dataTransfer.setData("parent", event.target.parentNode.id);
 }
 
+/**
+ * Handles the dropping of a Number Node.
+ * Places the Number Node within another Node and adds it to it's parent's tree.
+ * @param event
+ */
+function dropNumberNode(event) {
+    event.preventDefault();
+    const id = event.dataTransfer.getData("text");
+    const node = document.getElementById(id);
+
+    // Ensures that the parent is a Node
+    if (event.target.parentNode.classList.contains("node")) {
+        const clone = node.cloneNode(true);
+        clone.id = NodeFactory.createNode(clone.classList, true);
+        event.target.appendChild(clone);
+    }
+}
+
+/**
+ * Determines how to handle the dropping of each Node that is dragged into the editor space.
+ * @param event
+ */
 function dropNode(event) {
     event.preventDefault();
     const id = event.dataTransfer.getData("text");
-    const parentId = event.dataTransfer.getData("parent");
     const node = document.getElementById(id);
 
-    // Determine behavior based upon source dragged from
-    if (parentId !== "editorPane") {
-        const clone = node.cloneNode(true);
-        clone.id = NodeFactory.createNode(clone.classList, true);
-        clone.style.position = "fixed";
-        clone.style.top = `${event.pageY}px`;
-        clone.style.left = `${event.pageX}px`;
-        event.target.appendChild(clone);
+    // Handles Number Nodes separately than term Nodes
+    if (node.classList.contains("number")) {
+        dropNumberNode(event);
     }
     else {
-        node.style.position = "fixed";
-        node.style.top = `${event.pageY}px`;
-        node.style.left = `${event.pageX}px`;
-    }
+        // Create a clone of the Node if dragged from the selection list. Else just move it
+        if (event.target.id !== "editorPane") {
+            const clone = node.cloneNode(true);
+            clone.id = NodeFactory.createNode(clone.classList, true);
+            clone.style.position = "fixed";
+            clone.style.top = `${event.pageY}px`;
+            clone.style.left = `${event.pageX}px`;
+            event.target.appendChild(clone);
+        }
+        else {
+            node.style.position = "fixed";
+            node.style.top = `${event.pageY}px`;
+            node.style.left = `${event.pageX}px`;
+        }
 
-    Calculator.updateResult();
+        Calculator.updateResult();
+    }
 }
 
+/**
+ * Removes the Node from the HTML DOM as well as the NodeForest.
+ * @param event
+ */
 function deleteNode(event) {
     event.preventDefault();
-    const id = event.dataTransfer.getData("text");
-    document.getElementById(id).remove();
-    NodeForest.deleteRootNode(id);
+    if (event.target.parentNode.classList.contains("panel")) {
+        const id = event.dataTransfer.getData("text");
+        document.getElementById(id).remove();
+        NodeForest.deleteRootNode(id);
+    }
 }
 
+/**
+ * Allows the Nodes behavior when dropped to be manipulated.
+ * @param event
+ */
 function allowDroppingNode(event) {
     event.preventDefault();
 }
