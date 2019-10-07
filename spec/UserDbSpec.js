44c82c56f6f32c4bbb2187433262b26ed6a10879
Vatricia Edgar <vatriciaedgar@gmail.com>
2019-10-07 10:00:16 -0700

    Testing add user and login

---
+++ spec/UserDbSpec.js
@@ -0,0 +1,45 @@
+/* global describe */
+/* global it */
+/* global expect */
+
+/* Imports for test */
+const UserDb = require("../src/backend/UserDb.js");
+
+/* Write the test suite */
+describe("Usser DB", () => {
+    /* A setup function
+    beforeEach(() => {
+    });
+    */
+
+    /* A specification */
+    it("shall be able to retrieve a user with a correct username and password", async function() {
+        let res = await UserDb.addUser("vcedgar", "1234");
+        expect(res).toBe(true);
+
+        let result1 = await UserDb.userLogin("vcedgar", "1234");
+        let result2 = await UserDb.userLogin("vcedgar", "12345");
+        let result3 = await UserDb.userLogin("vcedgar6", "1234");
+        expect(result1).toBe(true);
+        expect(result2).toBe(false);
+        expect(result3).toBe(false);
+    });
+
+    /* A specification */
+    it("shall be able to add a user with a new username and fail at adding a user with an existing username", async function() {
+        let result1 = await UserDb.addUser("jebush", "1234");
+        let result1p2 = await UserDb.userLogin("jebush", "1234");
+
+        expect(result1 && result1p2).toBe(true);
+
+        let result2 = await UserDb.addUser("jebush", "12345");
+        let result2p2 = await UserDb.userLogin("jebush", "12345");
+        expect(result2).toBe(false);
+        expect(result2p2).toBe(false);
+
+        let result3 = await UserDb.addUser("jebush1", "1234");
+        let result3p2 = await UserDb.userLogin("jebush1", "1234");
+        expect(result3 && result3p2).toBe(true);
+
+    });
+});
--- src/backend/UserDb.js
+++ src/backend/UserDb.js
@@ -3,23 +3,24 @@ const Path = require("path");
 
 class UserDb {
     constructor() {
-        if (!UserDb.instance) {
-            const programDbPath = Path.relative(process.cwd(), "./data/ProgramDb.db");
-            this.programDb = NedDb({ filename: programDbPath, autoload: true });
-            UserDb.instance = this;
-        }
+        const programDbPath = Path.relative(process.cwd(), "./data/ProgramDb.db");
+        this.programDb = NedDb({ filename: programDbPath, autoload: true });
+        UserDb.instance = this;
         return UserDb.instance;
     }
 
-    async userExists(un, pw) {
-        const doc = await this.programDb.cfind({ username: un, password: pw })
-            .exec();
+    async userLogin(un, pw) {
+        const doc = await this.programDb.find({ username: un, password: pw });
+        return doc.length !== 0;
+    }
+    async userExists(un) {
+        const doc = await this.programDb.find({ username: un });
         return doc.length !== 0;
     }
 
     async addUser(un, pw) {
-        let doc = await this.programDb.cfind({ username: un}).exec()
-        if(doc.length !== 0){
+        let exists = await this.userExists(un);
+        if(exists){
             return false;
         }
         await this.programDb.insert({ username: un, password: pw });
