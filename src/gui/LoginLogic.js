b2a315bcb7269ff1075e4af5dfbe6a176eb7217a
Vatricia Edgar <vatriciaedgar@gmail.com>
2019-11-02 15:03:36 -0700

    Made scrolling table easy to insert with JS

--- src/gui/LoginLogic.js
+++ src/gui/LoginLogic.js
@@ -1,5 +1,6 @@
 /* global document */
 /* global UserDb */
+/* global pug */
 
 async function sendLoginRequest() {
     const username = document.getElementById("username").value;
@@ -17,6 +18,16 @@ async function sendLoginRequest() {
             document.getElementById("loginFail").hidden = true;
             document.getElementById("login").hidden = true;
             document.getElementById("AdminView").hidden = false;
+            const pugPath = Path.relative(process.cwd(), "./src/gui/pug/AdminViewStudentsScrollTable.pug");
+            const compiledFunction = pug.compileFile(pugPath);
+            const students = [['asd', 'asad', 'asd'], ['Empty', 'Empty', 'Empty'], ['Empty', 'Empty', 'Empty'],
+                ['123', '123', '123'], ['Empty', 'Empty', 'Empty'], ['Empty', 'Empty', 'Empty'],
+                ['Empty', 'Empty', 'Empty'], ['Empty', 'Empty', 'Empty'], ['Empty', 'Empty', 'Empty'],
+                ['Empty', 'Empty', 'Empty']];
+            const scrolledTable = compiledFunction({
+                students: students
+            });
+            document.getElementById("CourseEditorStudents").innerHTML = scrolledTable;
         }
     }
 }
