ff9d9f2266c910e1d31eac33eacbda43faa5b440
zlmonroe <zlmonroe@asu.edu>
2019-11-10 13:05:00 -0700

    US3:Task166 The teacher can add questions to the assignment and is able to choose a course. Need courselist from db

---
+++ src/gui/CreateAssignment.js
@@ -0,0 +1,26 @@
+/* global window */
+/* global document */
+
+function updateCourses(elem) {
+    if (elem.innerHTML !== "") {
+        return;
+    }
+    const userID = window.localStorage.getItem("userID");
+    const courses = ["SER334", "SER101", userID]; // CourseDb.getCourseNamesForTeacher(userID);
+    for (let i = 0; i < courses.length; i++) {
+        const option = document.createElement("option");
+        option.textContent = courses[i];
+        option.value = courses[i];
+        elem.appendChild(option);
+    }
+}
+
+function addQuestion(button) {
+    const forID = button.getAttribute("data-for");
+    const forElem = document.getElementById(forID);
+    forElem.innerHTML += "<li class='question'><label class='XButton' onclick='deleteQuestion(this)'>X</label><label>Q</label><textarea></textarea></li>";
+}
+
+function deleteQuestion(elem) {
+    elem.parentElement.parentElement.removeChild(elem.parentElement);
+}
--- src/gui/LoginLogic.js
+++ src/gui/LoginLogic.js
@@ -1,4 +1,5 @@
 /* global document */
+/* global window */
 /* global UserDb */
 /* global loadAllCoursesList */
 /* global loadCourseStudentList */
@@ -19,6 +20,8 @@ async function sendLoginRequest() {
         }
         else {
             const user = await UserDb.getUser(username);
+            window.localStorage.setItem("username", user.username);
+            window.localStorage.setItem("userID", user._id);
             document.getElementById("loginFail").hidden = true;
             document.getElementById("login").hidden = true;
             if (user.role === "admin") {
