3147b3f2c9a9b494b30b3b06fba69e15a126f3f0
majluni <majluni@asu.edu>
2019-11-19 21:17:52 -0700

    Comment out unneeded code

--- src/gui/Assignments.js
+++ src/gui/Assignments.js
@@ -15,11 +15,11 @@ class Assignment {
     }
 }
 
-const assignments = [
-    new Assignment("Deliverable 2", "", new Date(2019, 10, 4)),
-    new Assignment("Deliverable 3", "", new Date(2019, 11, 3)),
-    new Assignment("Final Test", "", new Date(2019, 10, 15)),
-];
+// const assignments = [
+//     new Assignment("Deliverable 2", "", new Date(2019, 10, 4)),
+//     new Assignment("Deliverable 3", "", new Date(2019, 11, 3)),
+//     new Assignment("Final Test", "", new Date(2019, 10, 15)),
+// ];
 
 /*
  Takes an array of assignment objects
