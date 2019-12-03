d90f83401029d07ab3293218567caab80f81af98
majluni <majluni@asu.edu>
2019-12-02 20:22:14 -0700

    Fix await in loop

--- src/gui/StudentView.js
+++ src/gui/StudentView.js
@@ -10,26 +10,24 @@ async function loadAssignmentGradesStudent() {
     const pugPath = Path.relative(process.cwd(), "./src/gui/pug/GradesList.pug");
     const compiledFunction = pug.compileFile(pugPath);
     const assignments = await AssignDb.loadCurrentAssignments();
-    const submissions = [];
     const user = window.localStorage.getItem("username");
-    for (let i = 0; i < assignments.length; i++) {
-        const assignment = assignments[i];
-        if (assignment !== undefined && assignment.submissions !== undefined) {
-            const users = await Promise.all(assignment.submissions.map(x =>
-                UserDb.getUserWithId(x.studentId)));
-            const usernames = users.map(x => x.username);
-            for (let j = 0; j < assignment.submissions.length; ++j) {
-                if (usernames[j] === user) {
-                    if (assignment.submissions[j].grade !== undefined) {
-                        submissions.push([assignment.name, `${assignment.submissions[j].grade} / ${assignment.points}`]);
-                    }
-                    else {
-                        submissions.push([assignment.name, `X / ${assignment.points}`]);
-                    }
-                }
+    const userID = window.localStorage.getItem("userID");
+    const submissions = assignments.filter(assignment => assignment.submissions !== undefined)
+        .map((assignment) => {
+            assignment.submissions.forEach((submission) => {
+                submission.name = assignment.name;
+                submission.points = assignment.points;
+            });
+            return assignment.submissions;
+        })
+        .flat()
+        .filter(submission => submission.studentId === userID)
+        .map((submission) => {
+            if (submission.grade !== undefined) {
+                return [submission.name, `${submission.grade} / ${submission.points}`];
             }
-        }
-    }
+            return [submission.name, `X / ${submission.points}`];
+        });
     const scrolledTable = compiledFunction({ name: user, submissions });
     const assignmentGrades = document.getElementById("assignment_grades");
     assignmentGrades.innerHTML = scrolledTable;
